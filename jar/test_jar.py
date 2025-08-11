1115dd784fd1dac4135b7d8fcde20bee0b3807bd
Winter-stone <marvinmontic7@gmail.com>
2025-08-11 08:05:03 +0200

    Project Submission

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -9,10 +9,26 @@ def test_str():
     jar.deposit(11)
     assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
-def test_erroes():
+def test_errors():
     jar = Jar()
     with pytest.raises(ValueError):
         jar.deposit(13)
 
     with pytest.raises(ValueError):
         jar.withdraw(1)
+
+    with pytest.raises(ValueError):
+        jar.capacity = -1
+
+def test_deposit():
+    jar = Jar()
+    assert jar.deposit(12) == 12
+
+def test_withdraw():
+    jar = Jar()
+    assert jar.deposit(12) == 12
+    assert jar.withdraw(10) == 2
+
+def test_init():
+    jar = Jar()
+    assert jar.capacity == 12
